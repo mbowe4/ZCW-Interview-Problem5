7d0e987bbe4ebca10d5ae6a9cfac2bb8f0d30bb5
mbowe4 <mlbowe@gmail.com>
2018-04-15 20:53:09 -0400

    complete

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,11 @@
 package io.zipcoder;
 
 public class Problem5 {
+    public String reverseString(String input) {
+        if (input.length() <= 1 ) {
+            return input;
+        }
+        return input.substring(input.length()-1, input.length()) + reverseString(input.substring(0, input.length()-1));
+    }
 }
+
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,51 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    @Test
+    public void reverseStringWithSpacesTest() {
+        Problem5 problem5 = new Problem5();
+        String input = "I love UNIT Testing";
+
+        String expected = "gnitseT TINU evol I";
+        String actual = problem5.reverseString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseEmptyStringTest() {
+        Problem5 problem5 = new Problem5();
+        String input = "";
+
+        String expected = "";
+        String actual = problem5.reverseString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseSingleLetterStringTest() {
+        Problem5 problem5 = new Problem5();
+        String input = "A";
+
+        String expected = "A";
+        String actual = problem5.reverseString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseStringNoSpacesTest() {
+        Problem5 problem5 = new Problem5();
+        String input = "Hello";
+
+        String expected = "olleH";
+        String actual = problem5.reverseString(input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
